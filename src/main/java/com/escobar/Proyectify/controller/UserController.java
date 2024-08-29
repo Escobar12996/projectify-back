55ba8ff39810e5c39f628b341c8fc15cd441c80e
Escobar12996 <escobarruedaantonio@outlook.com>
2024-08-29 23:44:02 +0200

    configuracion swagger
    Instalando swagger

---
+++ src/main/java/com/escobar/Proyectify/controller/UserController.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.escobar.Proyectify.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author escobar
+ */
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    @GetMapping
+    public List<String> getAllUsers() {
+        return new ArrayList<String>();
+    }
+}
