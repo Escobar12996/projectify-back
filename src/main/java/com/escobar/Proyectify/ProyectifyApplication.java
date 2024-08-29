2ee67b1e60156d3e5620eeb3d586d49ee950d31a
Escobar12996 <escobarruedaantonio@outlook.com>
2024-08-29 22:57:37 +0200

    Inicio
    Inicio de aplicacion

---
+++ src/main/java/com/escobar/Proyectify/ProyectifyApplication.java
@@ -0,0 +1,13 @@
+package com.escobar.Proyectify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/escobar/Proyectify/ProyectifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.escobar.Proyectify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
