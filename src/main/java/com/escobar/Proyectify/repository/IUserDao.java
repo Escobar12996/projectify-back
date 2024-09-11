cf9ec630be08480eff55e53dfa1061821b52a4e7
Escobar12996 <escobarruedaantonio@outlook.com>
2024-09-12 00:06:09 +0200

    Auth

--- src/main/java/com/escobar/Proyectify/repository/IUserDao.java
+++ src/main/java/com/escobar/Proyectify/repository/IUserDao.java
@@ -2,15 +2,16 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-package com.escobar.Proyectify.service;
+package com.escobar.Proyectify.repository;
 
 import com.escobar.Proyectify.model.User;
+import org.springframework.data.repository.CrudRepository;
 
 /**
  *
  * @author escobar
  */
-public interface UserService {
+public interface IUserDao extends CrudRepository<User, Long> {
 
     public User findByUsername(String username);
 
